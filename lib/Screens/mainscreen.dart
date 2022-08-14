805b3342fbd1ac6f279974b33172257193f3e2ad
Tokelo Mathabathe <Mathabathetokelo75@gmail.com>
2022-08-14 14:09:56 +0200

    Search, Home & Work address bars added

--- lib/Screens/mainscreen.dart
+++ lib/Screens/mainscreen.dart
@@ -26,13 +26,133 @@ class _MainScreen extends State<MainScreen> {
       body: Stack(
         children: [
           GoogleMap(
-              mapType: MapType.normal,
-              myLocationButtonEnabled: true,
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controllerGoogleMap.complete(controller);
-                newGoogleMapController = controller;
-              })
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newGoogleMapController = controller;
+            },
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 320.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 16.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Text(
+                      "Hi there, ",
+                      style: TextStyle(fontSize: 12.0),
+                    ),
+                    Text(
+                      "Where to? ",
+                      style:
+                          TextStyle(fontSize: 20.0, fontFamily: "Brand-Bold"),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      height: 50.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 6.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7),
+                          )
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.blueGrey,
+                          ),
+                          SizedBox(width: 10.0),
+                          Text("Search Drop Off"),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 24.0),
+                    Row(
+                      children: [
+                        Icon(Icons.home, color: Colors.grey),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Home"),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              "Your living home address",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 12.0),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Divider(),
+                    SizedBox(height: 16.0),
+                    Row(
+                      children: [
+                        Icon(Icons.work, color: Colors.grey),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Work"),
+                            SizedBox(
+                              height: 4.0,
+                            ),
+                            Text(
+                              "Your office address",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 12.0),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
