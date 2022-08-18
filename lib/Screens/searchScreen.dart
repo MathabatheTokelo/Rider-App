9c63b449fde4c44e186b813ff274e95f7e61275f
Tokelo Mathabathe <Mathabathetokelo75@gmail.com>
2022-08-18 15:47:29 +0200

    Enabled google places api - auto complete search

--- lib/Screens/searchScreen.dart
+++ lib/Screens/searchScreen.dart
@@ -1,5 +1,7 @@
+import 'package:final_year_project_rider_app/Assistants/request_assistant.dart';
 import 'package:final_year_project_rider_app/DataHandler/appData.dart';
 import 'package:final_year_project_rider_app/Screens/mainscreen.dart';
+import 'package:final_year_project_rider_app/configMaps.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'mainscreen.dart';
@@ -110,6 +112,9 @@ class _SearchScreenState extends State<SearchScreen> {
                           child: Padding(
                             padding: EdgeInsets.all(3.0),
                             child: TextField(
+                              onChanged: (val) {
+                                findPlace(val);
+                              },
                               controller: dropOffTextEditingController,
                               decoration: InputDecoration(
                                   hintText: "Where to ?",
@@ -132,5 +137,19 @@ class _SearchScreenState extends State<SearchScreen> {
         ],
       ),
     );
+  }
+
+  void findPlace(String placeName) async {
+    if (placeName.length > 1) {
+      String autoCompleteUrl =
+          "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$placeName&key=$mapKey&sessiontoken=";
+      var res = await RequestAssistant.getRequest(autoCompleteUrl);
+
+      if (res == "failed") {
+        return;
+      }
+      print("Places Predictions Response ::");
+      print(res);
+    }
   }
 }
